26cc1e355a4caccb83c360e5b3130f512050fff9
unknown <tharunn030103@gmail.com>
2025-12-03 18:28:34 +0530

    made changes

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const theme = document.getElementById("themebtn");
+const colorlight = "#9c9c9c"
+const colordark = "#222"
 
 function applyTheme() {
     // If the button currently says Dark, user wants dark mode
     if (theme.innerText === "Dark") {
-        document.documentElement.style.setProperty("--bgcolor", "#222");
-        document.documentElement.style.setProperty("--textcolor", "slategray");
+        document.documentElement.style.setProperty("--bgcolor", colordark);
+        document.documentElement.style.setProperty("--textcolor", colorlight);
         theme.innerText = "Light";
     } 
     else {
-        document.documentElement.style.setProperty("--bgcolor", "slategray");
-        document.documentElement.style.setProperty("--textcolor", "#222");
+        document.documentElement.style.setProperty("--bgcolor", colorlight);
+        document.documentElement.style.setProperty("--textcolor", colordark);
         theme.innerText = "Dark";
     }
 }
