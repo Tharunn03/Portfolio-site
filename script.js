452bffd833db1c5d2dfd213d3a17385ed955cc77
unknown <tharunn030103@gmail.com>
2025-12-03 16:38:30 +0530

    added day/night theme

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const theme = document.getElementById("themebtn");
 
-function applyTheme()
-{
-    if (theme == "dark")
-    {
-        
+function applyTheme() {
+    // If the button currently says Dark, user wants dark mode
+    if (theme.innerText === "Dark") {
+        document.documentElement.style.setProperty("--bgcolor", "#222");
+        document.documentElement.style.setProperty("--textcolor", "slategray");
+        theme.innerText = "Light";
+    } 
+    else {
+        document.documentElement.style.setProperty("--bgcolor", "slategray");
+        document.documentElement.style.setProperty("--textcolor", "#222");
+        theme.innerText = "Dark";
     }
 }
+
+theme.addEventListener("click", applyTheme);
